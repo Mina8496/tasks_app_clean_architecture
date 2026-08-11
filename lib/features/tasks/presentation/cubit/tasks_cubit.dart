ce2a1886cd41de6291a67adecdefde0e5d78dab0
Mina Kamil <mena.m4077@gmail.com>
2026-08-11 16:22:36 +0300

    refactor(tasks_bloc, tasks_state): add changeBottomSheetState add GetDataFromDatabase add changeIndex add createDatabase

---
+++ lib/features/tasks/presentation/cubit/tasks_cubit.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:tasks_app_clean_architecture/features/tasks/presentation/cubit/tasks_state.dart';
+import 'package:tasks_app_clean_architecture/features/tasks/presentation/pages/New_Tasks_page.dart';
+import 'package:tasks_app_clean_architecture/features/tasks/presentation/pages/archived_tasks_page.dart';
+import 'package:tasks_app_clean_architecture/features/tasks/presentation/pages/done_tasks_page.dart';
+
+class TasksCubit extends Cubit<TasksState> {
+  TasksCubit() : super(TasksInitial());
+
+  static TasksCubit get(context) => BlocProvider.of(context);
+
+  int currentIndex = 0;
+
+  List<Widget> screens = [NewTasksPage(), DoneTasksPage(), ArchivedTasksPage()];
+
+  List<String> titles = ['New Tasks', 'Done Tasks', 'Archived Tasks'];
+
+  void changeIndex(int index) {
+    currentIndex = index;
+    emit(ChangeBottomNavBarState());
+  }
+
+  late Database database;
+  List<Map> tasks = [];
+
+  void createDatabase() {
+    openDatabase(
+      'tasks.db',
+      version: 1,
+      onCreate: (database, version) {
+        print('Database created');
+        database
+            .execute(
+              'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, data TEXT, time TEXT, status TEXT)',
+            )
+            .then((value) {
+              print("create table");
+            })
+            .catchError((error) {
+              print("Error when creating table ${error.toString()}");
+            });
+      },
+      onOpen: (database) {
+        GetDataFromDatabase(database).then((value) {
+          tasks = value;
+          emit(GetTasksState());
+          print(tasks);
+        });
+        print('Database opened');
+      },
+    ).then((value) {
+      database = value;
+      emit(CreateTaskState());
+    });
+  }
+
+  Future insertToDatabase({
+    required String title,
+    required String data,
+    required String time,
+  }) async {
+    return await database.transaction((txn) {
+      txn
+          .rawInsert(
+            'INSERT INTO tasks(title, data, time, status) VALUES("$title", "$data", "$time", "new")',
+          )
+          .then((value) {
+            emit(InsertTaskState());
+            print("$value inserted successfully");
+          })
+          .catchError((error) {
+            print("Error when inserting new record ${error.toString()}");
+          });
+      return Future.value();
+    });
+  }
+
+  Future<List<Map>> GetDataFromDatabase(database) async {
+    return await database.rawQuery('SELECT * FROM tasks');
+  }
+
+  bool isBottomSheetShown = false;
+  IconData fabIcon = Icons.edit;
+
+  void changeBottomSheetState({required bool isShow, required IconData icon}) {
+    isBottomSheetShown = isShow;
+    fabIcon = icon;
+    emit(ChangeBottomSheetState());
+  }
+}
---
+++ lib/features/tasks/presentation/cubit/tasks_state.dart
@@ -0,0 +1,13 @@
+abstract class TasksState {}
+
+class TasksInitial extends TasksState {}
+
+class ChangeBottomNavBarState extends TasksState {}
+
+class CreateTaskState extends TasksState {}
+
+class InsertTaskState extends TasksState {}
+
+class GetTasksState extends TasksState {}
+
+class ChangeBottomSheetState extends TasksState {}
